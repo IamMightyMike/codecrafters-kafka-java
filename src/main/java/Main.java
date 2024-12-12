cc4642a407e0369be53ce983125b854cf4e55fa6
IamMightyMike <m_ochoa_chinchetru@yahoo.es>
2024-12-12 21:28:43 +0100

    checked solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 import java.util.Arrays;
@@ -32,26 +33,38 @@ public class Main {
 
       //Para conectar desde WSL sacar la IP con cat /etc/resolv.conf | grep nameserver
 
-      byte[] buffer = new byte[1024];
-      int byteRead;
-      if ((byteRead = input.read(buffer)) != -1) {
+      //byte[] buffer = new byte[1024];
+      //int byteRead;
+      //if ((byteRead = input.read(buffer)) != -1) {
+//
+//
+      //    byte[] output = new byte[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//
+      //    int offsetOrigin = 8;
+      //    int offsetDestination = 4;
+      //    IntStream.range(0,6).forEach(i -> {
+//
+      //    output[i + offsetDestination] = buffer[i + offsetOrigin];
+      //  });
+//
+      //  System.arraycopy(buffer, 8, output, 4, 6);
+      //  out.write(output);
+      //} else {
+      //  System.out.println("Nothing to read from input stream");
+      //}
 
+      byte[] length = input.readNBytes(4);
+      byte[] apiKey = input.readNBytes(2);
+      byte[] apiVersion = input.readNBytes(2);
+      short shortApiVersion = ByteBuffer.wrap(apiVersion).getShort();
+      byte[] correlationId = input.readNBytes(4);
 
-          byte[] output = new byte[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      out.write(length);
+      out.write(correlationId);
+      if (shortApiVersion < 0 || shortApiVersion > 4) {
 
-          int offsetOrigin = 8;
-          int offsetDestination = 4;
-          IntStream.range(0,6).forEach(i -> {
-
-          output[i + offsetDestination] = buffer[i + offsetOrigin];
-        });
-
-        System.arraycopy(buffer, 8, output, 4, 6);
-        out.write(output);
-      } else {
-        System.out.println("Nothing to read from input stream");
+        out.write(new byte[] {0, 35});
       }
-
 
     }catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
