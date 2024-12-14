cdbe75b2db86d19f7d5f15f73760c6d27d8953ad
IamMightyMike <m_ochoa_chinchetru@yahoo.es>
2024-12-14 21:41:25 +0100

    Further progress and cleanup chores

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+
+    public static final int ERROR_CODE_OK = 0;
+    public static final int ERROR_CODE_UNSUPPORTED_VERSION = 35;
+    public static final int API_KEY_REQUEST = 18;
+    public static final int MAX_API_VERSION = 18;
+    public static final int MIN_API_VERSION = 18;
+
+}
--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -3,6 +3,7 @@ package utils;
 import dto.Request;
 import dto.Response;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -26,9 +27,9 @@ public class Parser {
 
         Response response = Response.builder()
                 .setCorrelationId(request.getCorrelationId())
-                .setErrorCode(request.getApiVersion() < 0 ||  request.getApiVersion() >4 ?(short) 35:(short)0)
+                .setErrorCode(request.getApiVersion() < 0 ||  request.getApiVersion() >4 ?(short) Constants.ERROR_CODE_UNSUPPORTED_VERSION:(short)Constants.ERROR_CODE_OK)
                 .setArrayLength(2)
-                .setApiKey((short) 18)
+                .setApiKey((short) Constants.API_KEY_REQUEST)
                 .setMaxVersion((short)4)
                 .setMinVersion((short)3)
                 .setArrayTaggedFields((byte)0)
