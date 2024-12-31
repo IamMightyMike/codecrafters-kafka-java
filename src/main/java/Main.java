8c59e912054af0fec3d759cfb627b45f44c771df
IamMightyMike <m_ochoa_chinchetru@yahoo.es>
2024-12-31 01:56:06 +0100

    Push to github

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,6 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
 
-
-
-
     try {
       Server server = new Server(9092);
       server.run();
